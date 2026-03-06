25f3a8cf9c85386ff41d97f3aad64f254a152608
Moiz Ansari <moizansari2005@gmail.com>
2026-03-06 12:01:23 +0500

    Added UserModel.js

---
+++ Task 3 (Login System)/UserModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+// Schema = define the shape of a user document
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+// Model = your interface to the "users" collection
+const UserModel = mongoose.model('User', userSchema);
+
+module.exports = UserModel;
--- Task 3 (Login System)/db.js
+++ Task 3 (Login System)/db.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
+        //await mongoose.connect('mongodb://localhost:27017/studentDB');    // in case online doesnt work, use local MongoDB
         console.log('MongoDB connected');
     } catch (err) {
         console.error('MongoDB connection error:', err);
