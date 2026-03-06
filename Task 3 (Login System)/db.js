10513f61d3624fde6ccd0ab6a2248f887529321e
Moiz Ansari <moizansari2005@gmail.com>
2026-03-06 11:55:34 +0500

    Setup Secure Online MongoDB Atlas Connection

--- Task 3 (Login System)/db.js
+++ Task 3 (Login System)/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Connect to MongoDB
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/studentDB');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected');
     } catch (err) {
         console.error('MongoDB connection error:', err);
