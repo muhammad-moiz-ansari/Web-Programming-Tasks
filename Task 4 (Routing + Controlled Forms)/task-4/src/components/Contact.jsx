f2b9aaa3494ca61bc696331516e064d364945c2c
Moiz Ansari <moizansari2005@gmail.com>
2026-03-19 14:59:44 +0500

    Added static contact form

--- Task 4 (Routing + Controlled Forms)/task-4/src/components/Contact.jsx
+++ Task 4 (Routing + Controlled Forms)/task-4/src/components/Contact.jsx
@@ -1,10 +1,35 @@
 function Contact() {
-  return (
-    <section style={{ padding: '20px', backgroundColor: '#4a0000' }}>
-      <h2>Contact Us</h2>
-      <p>Feel free to reach out via email or GitHub!</p>
-    </section>
-  );
+	return (
+		<section id="contact-section">
+			<section id="form-container">
+				<h2>Contact Us</h2>
+				<div className="contact-form">
+					<form>
+						<div className="contact-item">
+							<label>
+								<p><i className="fa fa-user"></i> Name:</p>
+								<input type="text" name="name" placeholder="Your Name" required /><br /><br />
+							</label>
+						</div>
+						<div className="contact-item">
+							<label>
+								<p><i className="fa fa-envelope"></i> Email:</p>
+								<input type="email" name="email" placeholder="e.g. domain@gmail.com" required /><br /><br />
+							</label>
+						</div>
+						<div className="contact-item">
+							<p>Message:</p>
+							<textarea placeholder="Write message here"></textarea>
+						</div>
+						<br />
+						<div className="contact-item">
+							<button type="submit">Submit</button>
+						</div>
+					</form>
+				</div>
+			</section>
+		</section>
+	);
 }
 
 export default Contact;
