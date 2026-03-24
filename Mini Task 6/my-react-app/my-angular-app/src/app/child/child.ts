89ad08cdf25599a02ed357003585228d505a23f1
Moiz Ansari <moizansari2005@gmail.com>
2026-03-24 13:02:59 +0500

    Setup Angular Project & Passed data to Child Component

---
+++ Mini Task 6/my-react-app/my-angular-app/src/app/child/child.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  standalone: true,
+  imports: [],
+  templateUrl: './child.html',
+  styleUrl: './child.css',
+})
+export class Child {
+  @Input() message: string = '';
+}
